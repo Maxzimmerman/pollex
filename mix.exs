143fa229503aa843194d13ccaa02d93ead137ab6
Maxzimmerman <maxw.zim@gmail.com>
2025-07-17 13:37:52 +1000

    Added documentation.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule Pollex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "An Elixir polling and HTTP abstraction library",
+      package: package(),
+      name: "pollex",
+      source_url: "https://github.com/https://github.com/Maxzimmerman/pollex",
+      docs: [main: "readme", extras: ["README.md"]]
     ]
   end
 
@@ -25,6 +30,16 @@ defmodule Pollex.MixProject do
       {:req, "~> 0.5.0"},
       {:mox, "~> 1.2.0", only: :test},
       {:ex_doc, "~> 0.12"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Max Zimmermann"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/Maxzimmerman/pollex"
+      }
     ]
   end
 end
