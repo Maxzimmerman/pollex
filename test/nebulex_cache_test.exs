8adbef7992b6e24f92c49db634621bd70f62c05b
Maxzimmerman <maxw.zim@gmail.com>
2025-10-29 18:10:48 +1100

    Added test for nebulex test.

--- test/nebulex_cache_test.exs
+++ test/nebulex_cache_test.exs
@@ -5,6 +5,58 @@ defmodule NebulexCacheTest do
   alias Pollex.{Repo, City}
   alias Pollex.NebulexCache
 
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+    Ecto.Adapters.SQL.Sandbox.mode(Repo, {:shared, self()})
+
+    # Insert some test data
+    Repo.insert!(%City{name: "germany"})
+    Repo.insert!(%City{name: "usa"})
+
+    # Setup GenServer
+    name = :"poller_test_#{System.unique_integer([:positive])}"
+    source_opts = [table: City, repo: Repo]
+    cache_opts = [columns: [:name]]
+    interval = 1
+    sub_cache_opts = [
+      gc_interval: :timer.hours(12),
+      max_size: 1_000_000,
+      allocated_memory: 2_000_000_000,
+      gc_cleanup_min_timeout: :timer.seconds(10),
+      gc_cleanup_max_timeout: :timer.minutes(10)
+    ]
+
+    pid =
+      start_supervised!(
+        {NebulexCache,
+         [
+          name: name,
+          source_opts: source_opts,
+          cache_opts: cache_opts,
+          refresh_rate: interval,
+          cache_runtime_opts: sub_cache_opts
+         ]}
+      )
+
+    {:ok, name: name, pid: pid}
+  end
+
+  describe "test the cache itself" do
+      test "state updates after poll", %{name: name} do
+      Process.sleep(100)
+
+      data = NebulexCache.lookup(name)
+      assert Enum.any?(data, &(&1.name == "germany"))
+    end
+
+    test "initial state loads from DB", %{name: name} do
+      Process.sleep(100)
+      data = NebulexCache.lookup(name)
+      assert Enum.any?(data, &(&1.name == "germany"))
+      assert Enum.any?(data, &(&1.name == "usa"))
+    end
+  end
+
   describe "transform_to_nebulex_format/1" do
     test "transforms list of maps into a map keyed by the first value string of each map" do
       data = [
